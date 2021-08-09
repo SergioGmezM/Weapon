733d837b822f3fd78842f67da87a9433bd397b9d
TheBender10 <sergiogomez9867@gmail.com>
2021-08-09 13:04:34 +0200

    "Implementado prototipo de disparo láser principal"

---
+++ Weapon/Assets/Scripts/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    [SerializeField] Transform gunBarrelTransform;
+    [SerializeField] Transform satelliteWeaponTransform;
+
+    [SerializeField] GameObject LaserBeam;
+
+    private bool isFiring = false;
+    private float range = 500f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isFiring = true;
+            FireLaserBeam();
+        }
+        else
+        {
+            isFiring = false;
+        }
+
+        if (!isFiring)
+        {
+            LaserBeam.SetActive(false);
+        }
+    }
+
+    private void FireLaserBeam()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(gunBarrelTransform.position, gunBarrelTransform.forward, out hit))
+        {
+            LineRenderer lr = LaserBeam.GetComponent<LineRenderer>();
+            lr.SetPositions(new Vector3[2] { satelliteWeaponTransform.position, hit.point });
+            LaserBeam.SetActive(true);
+        }
+        else
+        {
+            isFiring = false;
+        }
+    }
+}
